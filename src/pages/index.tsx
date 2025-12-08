8af35e64fab9e78e05a72d36ffa7921019316d80
seelx3 <seelx3@gmail.com>
2025-12-09 04:54:50 +0900

    プレースホルダ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,39 @@ import Cytoscape from "cytoscape";
 import type { HeadFC, PageProps } from "gatsby";
 
 const IndexPage: React.FC<PageProps> = () => {
-  const [inputData, setInputData] = React.useState("");
+  const [inputData, setInputData] = React.useState(`15 16
+0 1 head 0.5
+0 2 tail 0.5
+1 3 head 0.5
+1 4 tail 0.5
+2 5 head 0.5
+2 6 tail 0.5
+3 7 head 0.5
+3 8 tail 0.5
+4 9 head 0.5
+4 10 tail 0.5
+5 11 head 0.5
+5 12 tail 0.5
+6 13 head 0.5
+6 14 tail 0.5
+7 1 head2 1.0
+14 2 tail2 1.0
+0 {toss. toss. toss. coin([]).}
+1 {toss. toss. coin([h]).}
+2 {toss. toss. coin([t]).}
+3 {toss. coin([h,h]).}
+4 {toss. coin([t,h]).}
+5 {toss. coin([h,t]).}
+6 {toss. coin([t,t]).}
+7 {coin([h,h,h]).}
+8 {coin([t,h,h]).}
+9 {coin([h,t,h]).}
+10 {coin([t,t,h]).}
+11 {coin([h,h,t]).}
+12 {coin([t,h,t]).}
+13 {coin([h,t,t]).}
+14 {coin([t,t,t]).}
+`);
   const [selectedState, setSelectedState] = React.useState<{ id: string; label: string } | null>(null);
   const containerRef = React.useRef<HTMLDivElement | null>(null);
 
